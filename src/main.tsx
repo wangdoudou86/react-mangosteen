9ff9ae03d23c0bb1caea94261181c0e5929913d9
darkti <892184375@qq.com>
2023-04-01 15:51:57 +0800

    初始化代码

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { App } from './App'
+const div = document.getElementById('root') as HTMLElement
+const root = ReactDOM.createRoot(div)
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
