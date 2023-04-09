f6484b54895e1828ab70bc6e09dcbe07a64d60b8
darkti <892184375@qq.com>
2023-04-09 19:28:40 +0800

    添加路由页面

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
-const div = document.getElementById('root') as HTMLElement
-const root = ReactDOM.createRoot(div)
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/router'
 
+const div = document.getElementById('root') as HTMLElement
+
+const root = ReactDOM.createRoot(div)
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
