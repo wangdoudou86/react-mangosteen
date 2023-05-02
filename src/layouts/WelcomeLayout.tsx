f6c1d0d58fd2929fca2fab16603ba901d5b40b40
darkti <892184375@qq.com>
2023-05-02 19:17:48 +0800

    有bug的路由过渡动画

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,6 +1,22 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { animated, useTransition } from '@react-spring/web'
 export const WelcomeLayout: React.FC = () => {
-  return <div>
-    <Outlet />
-  </div>
+  const location = useLocation()
+  const transitions = useTransition(location.pathname, {
+    // 进入状态
+    from: { transform: 'translateX(100%)' },
+    // 稳定状态
+    enter: { transform: 'translateX(0%)' },
+    // 退出状态
+    leave: { transform: 'translateX(-100%)' },
+    config: { duration: 5000 }
+  })
+
+  return transitions((style, pathname) => {
+    return <animated.div key={pathname} style={style}>
+      <div style={{ textAlign: 'center' }}>
+        <Outlet />
+      </div>
+    </animated.div>
+  })
 }
