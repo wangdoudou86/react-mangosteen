134fea8088c58202240b96706871155756d053c2
darkti <892184375@qq.com>
2023-12-18 14:56:06 +0800

    feat: 添加unocss的配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/router'
+import { router } from './routes/router';
+import 'virtual:uno.css'
 
 const div = document.getElementById('root') as HTMLElement
 
