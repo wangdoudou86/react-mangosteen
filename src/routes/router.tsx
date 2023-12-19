8fbb033e074cd206fe8ced0119d682309c2560ee
darkti <892184375@qq.com>
2023-12-19 18:14:32 +0800

    feat: 添加404页面

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { RedirectToWelcome1 } from '../components/RedirectToWelcome1'
 import { MainLayout } from '../layouts/MainLayout'
+import { NotFoundPage } from '../pages/NotFoundPage'
 import { welcomeRoutes } from './welcomeRoutes'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
-    errorElement: <RedirectToWelcome1 />,
+    errorElement: <NotFoundPage />,
     children: [
       welcomeRoutes
     ],
