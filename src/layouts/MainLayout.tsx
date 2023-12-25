71c47b8961f89da49d996baeea294a5d88967b4c
darkti <892184375@qq.com>
2023-12-25 11:20:00 +0800

    fix: 调整路由

--- src/layouts/MainLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-export const MainLayout: React.FC = () => {
-  const hasRead = localStorage.getItem('hasReadWelcomes')
-  if (hasRead === 'yes') {
-    return <Navigate to="/home" />
-  } else {
-    return <div><Outlet /></div>
-  }
-}
---
+++ src/layouts/Root.tsx
@@ -0,0 +1,10 @@
+import { Navigate } from 'react-router-dom'
+export const Root: React.FC = () => {
+  const hasRead = localStorage.getItem('hasReadWelcomes')
+  if (hasRead === 'yes') {
+    return <Navigate to="/home" />
+  } else {
+    // return <div><Outlet /></div>
+    return <Navigate to="/welcome/1" />
+  }
+}
