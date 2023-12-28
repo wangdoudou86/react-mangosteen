f14afe95189785c37e7166ebb91b3970d05a52ff
darkti <892184375@qq.com>
2023-12-28 14:35:17 +0800

    feat: 1、封装Axios；2、开发环境和生产环境访问不同的域名

---
+++ src/lib/ajax.tsx
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+// npm run dev 开发 /api/v1/me vite-plugin-mock插件会自动找到mock目录下mock的路径
+// npm run build 上线 http://121.196.236.94:8080/api/v1
+axios.defaults.baseURL = isDev ? '/' : 'http://121.196.236.94:8080/api/v1'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.timeout = 10000
+
+export const ajax = {
+  get: (path: string) => {
+    return axios.get(path)
+  },
+  post: () => { },
+  patch: () => { },
+  delete: () => { },
+}
