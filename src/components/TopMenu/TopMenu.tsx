897af728ada2ea7ff7e47bb2f1b23348a5271be5
darkti <892184375@qq.com>
2024-03-25 16:04:29 +0800

    fix: 给侧边栏菜单添加滑动动画

--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -1,21 +1,56 @@
+import { animated, useSpring } from '@react-spring/web'
+import { useState } from 'react'
 import { CurrentUser } from './CurrentUser'
 import { Menu } from './Menu'
 
 interface Props {
   onClickMask?: () => void
+  visible?: boolean
 }
 export const TopMenu: React.FC<Props> = (props) => {
-  const { onClickMask } = props
+  const { onClickMask, visible } = props
+  const [maskVisible, setMaskVisible] = useState(false)
+  const maskStyles = useSpring({
+    opacity: visible ? 1 : 0,
+    onStart: ({ value }) => {
+      // 动画开始时
+      // 显示时，opacity 0 -> 1
+      // 隐藏时，opacity 1 -> 0
+      // 为什么不等于0呢？因为打印出来的opacity值不是0啊啊啊啊
+      if (value.opacity < 0.1) {
+        setMaskVisible(true)
+      }
+    },
+    onRest: ({ value }) => {
+      // 动画结束时
+      // 动画快结束时，当opacity值小于0.1，说明是在隐藏
+      if (value.opacity < 0.1) {
+        setMaskVisible(false)
+      }
+    },
+  })
+
+  const styles = {
+    ...maskStyles,
+    visibility: (maskVisible ? 'visible' : 'hidden') as 'visible' | 'hidden'
+  }
+
+  const menuStyles = useSpring({
+    opacity: visible ? 1 : 0,
+    transform: visible ? 'translateX(0%)' : 'translateX(-100%)',
+  })
   return (
     <>
-      <div fixed top-0 left-0 w="100%" h="100%" className="bg-black:75"
-        z="[calc(var(--z-menu)-1)]" onClick={onClickMask}
+      <animated.div fixed top-0 left-0 w="100%" h="100%" className="bg-black:75"
+        z="[calc(var(--z-menu)-1)]"
+        style={styles}
+        onClick={onClickMask}
       />
-      <div fixed top-0 left-0 w="70vw" max-w-20em h-screen flex flex-col b-3px b-red
-        z="[var(--z-menu)]">
+      <animated.div fixed top-0 left-0 w="70vw" max-w-20em h-screen flex flex-col b-3px b-red
+        z="[var(--z-menu)]" style={menuStyles}>
         <CurrentUser className="grow-0 shrink-0" />
         <Menu className="grow-1 shrink-1" />
-      </div>
+      </animated.div>
     </>
   )
 }
--- src/pages/ItemsPage/ItemsPage.tsx
+++ src/pages/ItemsPage/ItemsPage.tsx
@@ -46,9 +46,7 @@ export const ItemsPage: React.FC = () => {
       <ItemsSummary />
       <ItemsList items={items} />
       <AddItemFloatButton />
-      {
-        visible ? <TopMenu onClickMask={() => { setVisible(false) }} /> : null
-      }
+      <TopMenu visible={visible} onClickMask={() => { setVisible(false) }} />
     </div>
   )
 }
